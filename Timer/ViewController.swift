7165513bac90d985f6b672e1de333be749be3824
BABE <supakorn.wo@ku.th>
2017-09-25 23:18:55 +0700

    Finish Timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var LapsTime: UITextView!
@@ -23,35 +23,42 @@ class ViewController: UIViewController {
     var stringToAppend = ""
     var numberOfPeople = 0
     
+   
+    let Timehour = Calendar.current.component(.hour, from: Date())
+    let Timeminutes = Calendar.current.component(.minute, from: Date())
+    let Timeseconds = Calendar.current.component(.second, from: Date())
     @IBAction func Stop(_ sender: UIButton) {
-        isRunning = false
-        countSeconds = 0
-        countMinutes = 0
-        countHours = 0
-        Second.text = String(format:"%02d",countSeconds)
-        Minute.text = String(format:"%02d",countMinutes)
-        Hours.text = String(format:"%02d",countHours)
-        People.text = "0"
-        
+        if(isRunning){
+            isRunning = false
+            countSeconds = 0
+            countMinutes = 0
+            countHours = 0
+            Second.text = String(format:"%02d",countSeconds)
+            Minute.text = String(format:"%02d",countMinutes)
+            Hours.text = String(format:"%02d",countHours)
+            People.text = "0"
+            LapsTime.text =  LapsTime.text! + String(format:"Ended At %02d:%02d:%02d\nTotal People %d",Calendar.current.component(.hour, from: Date()),Calendar.current.component(.minute, from: Date()),Calendar.current.component(.second, from: Date()),numberOfPeople)
+        }
     }
     @IBAction func Start(_ sender: UIButton) {
-        isRunning = true
-        runTimer()
+        if(!isRunning){
+            isRunning = true
+            LapsTime.text =  LapsTime.text! + String(format:"Started At %02d:%02d:%02d\n",Calendar.current.component(.hour, from: Date()),Calendar.current.component(.minute, from: Date()),Calendar.current.component(.second, from: Date()))
+            runTimer()
+        }
     }
     @IBAction func Lap(_ sender: UIButton) {
-        numberOfPeople += 1
-        People.text = String(numberOfPeople)
-        LapsTime.text =  LapsTime.text! + stringToAppend
+        if(isRunning){
+            numberOfPeople += 1
+            People.text = String(numberOfPeople)
+            LapsTime.text =  LapsTime.text! + stringToAppend
+        }
     }
-    
-    
-    
     func runTimer() {
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self,
-                                         selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
-    func updateTimer(){
+    @objc func updateTimer(){
         if(isRunning){
             countSeconds += 1
         }
@@ -75,14 +82,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        LapsTime.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
